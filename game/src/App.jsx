2227fe9920d921ae62735d9bee3c73ed40a66f0e
VINÍCIUS SOARES FRANÇA <vinicius_s_franca@estudante.sesisenai.org.br>
2023-11-10 09:21:25 -0300

    Nature-Cards

---
+++ game/src/App.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Carta from './Components/Carta/Carta';
+import './App.css';
+
+export default () => (
+  <div className="App">
+  </div>
+);
+;
---
+++ game/src/Components/Carta/Carta.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import './Carta.css';
+
+export default () => (
+  <div className="Carta">
+  </div>
+);
