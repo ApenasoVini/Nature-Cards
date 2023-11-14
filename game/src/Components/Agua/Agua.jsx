498a85e789155581d9f23e8b8a8e82213595dfa8
ApenasoVini <viniciusicmsc@gmail.com>
2023-11-13 21:00:39 -0300

    CSS

--- game/src/Components/Agua/Agua.jsx
+++ game/src/Components/Agua/Agua.jsx
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactComponent as Icon } from "./agua.svg";
 import "./Agua.css"
 
-export default () => {
+const Agua = () => {
+    const [frente, mudarLado] = useState(true);
+
+    const clique = () => {
+        mudarLado(!frente);
+    };
+
     return (
-        <div className="Agua">
-            <div className="Background">
-                <p>Água</p>
-                <Icon className="Icon"></Icon>
-            </div>
+        <div className="Agua" onClick={clique}>
+            {frente ? (
+                <React.Fragment>
+                    <p className="Nome">Água</p>
+                    <Icon className="Icon" />
+                </React.Fragment>
+            ) : (
+                <p className="Descricao">Texto</p>
+            )}
         </div>
-    )
+    );
 };
 
+export default Agua;
