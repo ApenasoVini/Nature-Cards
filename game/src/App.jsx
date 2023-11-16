08489ac313f43e5dc8d2fa0f6f9824f5068c66aa
vi058587 <vinicius.franca@intelbras.com>
2023-11-16 17:10:00 -0300

    CSS

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -64,10 +64,10 @@ const App = () => {
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
             </p>
           </div>
-          <div className='Footer-Copyright'>
+        </div>
+        <div className='Footer-Copyright'>
             <p>&copy; 2023 Nature Cards. Todos os direitos reservados.</p>
           </div>
-        </div>
       </footer>
     </div>
   );
