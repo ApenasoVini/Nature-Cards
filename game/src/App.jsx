c8f885d93994517dcddcf134a0bef565b332c504
vi058587 <vinicius.franca@intelbras.com>
2023-11-16 17:05:16 -0300

    CSS

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -52,6 +52,21 @@ const App = () => {
               <i className='fa-brands fa-linkedin'></i>
             </a>
           </div>
+          <div className='Footer-App'>
+            <h1>Sobre o App</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className='Footer-Criador'>
+            <h1>Sobre Mim</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+          <div className='Footer-Copyright'>
+            <p>&copy; 2023 Nature Cards. Todos os direitos reservados.</p>
+          </div>
         </div>
       </footer>
     </div>
