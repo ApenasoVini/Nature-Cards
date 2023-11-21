882e5de4b04dade413702267b7c1ff951da1949b
Vinícius França ♜ <viniciusicmsc@gmail.com>
2023-11-21 17:13:42 -0300

    Update App.jsx

--- game/src/App.jsx
+++ game/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Agua from './Components/Primarios/Agua/Agua';
 import Ar from './Components/Primarios/Ar/Ar';
 import Fogo from './Components/Primarios/Fogo/Fogo';
